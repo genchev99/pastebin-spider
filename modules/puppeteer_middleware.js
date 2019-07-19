f67b0e90ddbfceb88854b1869d82250f56d59565
Hristiyan Genchev <hgenchev99@users.noreply.github.com>
2019-07-19 16:55:02 +0300

    Pup middlware page goto

--- modules/puppeteer_middleware.js
+++ modules/puppeteer_middleware.js
@@ -1,6 +1,41 @@
 "use strict";
 
 /**
+ * Closes the exiting page and then creates new page and load the page
+ * @param page Puppeteer page instance
+ * @param url The url to be loaded
+ * @param opts Load options
+ * @returns {Promise<any>}
+ */
+exports.pageGoto = (page, url, opts = {waitUntil: "domcontentloaded", timeout: 200000}) =>
+    new Promise(async (resolve, reject) => {
+      try {
+        /* Getting the browser instance from the page */
+        const browser = await page.browser();
+
+        /* Closing the page */
+        await this.pageClose(page);
+
+        /* Checking if the page is closed */
+        const isClosed = await page.isClosed();
+        console.log(`Page closed status: ${isClosed.toString()}`);
+
+        /* Creating the new page instance */
+        page = await browser.newPage();
+
+        /* Loading the new page */
+        await page.goto(url, opts);
+
+        return resolve(page);
+      } catch (e) {
+        /* If error is thrown the Promise will reject */
+        console.error(e);
+
+        return reject(e);
+      }
+    });
+
+/**
  * Clears the resources taken from the page
  * @param page Puppeteer page instance
  * @returns {Promise<any>}
