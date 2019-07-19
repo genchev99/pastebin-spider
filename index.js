6b945ade043b6a26c426b0fe9f9f7281f5dd75d1
Hristiyan Genchev <hgenchev99@users.noreply.github.com>
2019-07-19 17:15:57 +0300

    Fresh links scraper

--- index.js
+++ index.js
@@ -19,13 +19,70 @@ puppeteer.use(
 const mongoose = require("mongoose");
 const connectionOpts = {useNewUrlParser: true};
 
-mongoose.connect(process.env.DATABASE_URL, connectionOpts).then(() => {
+/* Modules */
+const middlware = require("./modules/puppeteer_middleware");
+
+/* Models */
+const paste = require("./models/paste");
+
+mongoose.connect(process.env.DATABASE_URL, connectionOpts).then(async () => {
   console.log(`Database connected on ${process.env.DATABASE_URL}`);
 
+  const browser = await createBrowser();
+
+  const links = await getPasteLinks(browser);
+  console.log(links);
+  
+  await browser.close();
 
 }).catch(err => {
   console.error(err, "Database connection error");
 
   process.exit(1065);
+});
+
+/**
+ * Creates new puppeteer browser instance
+ * @returns {Promise<any>}
+ */
+const createBrowser = () => new Promise(async (resolve, reject) => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    ignoreHTTPSErrors: true
+  }).catch(err => {
+    return reject(err);
+  });
+
+  return resolve(browser);
+});
+
+/**
+ * Scrapes the paste links on the archive page
+ * @param browser
+ * @returns {Promise<any>}
+ */
+const getPasteLinks = browser => new Promise(async (resolve, reject) => {
+  try {
+    /* Creating new page */
+    let page = await browser.newPage();
+
+    page = await middlware.pageGoto(page, "https://pastebin.com/archive");
+
+    /* Getting all fresh links */
+    const links = await page.evaluate(() => {
+      const rows = document.querySelectorAll("td:not(.td_smaller) a");
+      let links = [];
+
+      for (const row of rows) {
+        links.push(row.href);
+      }
+
+      return links;
+    });
+
+    return resolve(links);
+  } catch (e) {
+    return reject(e);
+  }
 });
 
